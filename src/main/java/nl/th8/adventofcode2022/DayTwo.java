3f7485347bef6767d5cc350b5d9dcc5af1bf1d88
th8 <tspel@live.nl>
2023-12-02 10:54:19 +0100

    Solved day 2

---
+++ src/main/java/nl/th8/adventofcode2022/DayTwo.java
@@ -0,0 +1,73 @@
+package nl.th8.adventofcode2022;
+
+import nl.th8.adventofcode2022.utils.PuzzleInputParser;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DayTwo implements Day {
+    private final PuzzleInputParser puzzleInputParser;
+
+    private final Map<String, Integer> partOneBounds = Map.of("red", 12, "green", 13, "blue", 14);
+
+    //For actual use
+    public DayTwo() {
+        puzzleInputParser = new PuzzleInputParser(Path.of("src", "main", "resources", "input", "2.txt"));
+    }
+
+    //For Unittesting
+    public DayTwo(PuzzleInputParser puzzleInputParser) {
+        this.puzzleInputParser = puzzleInputParser;
+    }
+
+    public int solvePartOne() {
+        List<String> lines = puzzleInputParser.getInputAsStringList();
+        int sumOfPossibleIds = 0;
+
+        for(String line : lines) {
+            //Split the game into it's ID and the game data
+            String[] gameSplit = line.split(": ", 2);
+            int gameId = Integer.parseInt(gameSplit[0].replace("Game ", ""));
+
+            //Split the game data on all comma's and semicolons and check if any are out of the game bounds
+            if(Arrays.stream(gameSplit[1].split("([,;] )"))
+                    .noneMatch(cubeSet -> {
+                        var cubeSetSplit = cubeSet.split(" ");
+                        return partOneBounds.get(cubeSetSplit[1]) < Integer.parseInt(cubeSetSplit[0]);
+                    })
+            ) {
+                sumOfPossibleIds += gameId;
+            }
+        }
+        System.out.println("The sum of possible game id's is " + sumOfPossibleIds);
+        return sumOfPossibleIds;
+    }
+
+    public int solvePartTwo() {
+        List<String> lines = puzzleInputParser.getInputAsStringList();
+        int sumOfAllPowerlevels = 0;
+
+        for(String line : lines) {
+            //Split the game into it's ID and the game data, discard the ID part.
+            String[] gameSplit = line.split(": ", 2);
+            Map<String, Integer> minimumCubesRequired = new HashMap<>();
+
+            //Split the game data on all comma's and semicolons and save the amount of cubes to a map if they're bigger than the previous biggest set of a certain colour
+            for(var cubeSet : gameSplit[1].split("([,;] )")) {
+                var cubeSetSplit = cubeSet.split(" ");
+                int amountOfCubes = Integer.parseInt(cubeSetSplit[0]);
+                minimumCubesRequired.compute(cubeSetSplit[1], (key, oldValue) -> oldValue == null ? amountOfCubes : amountOfCubes > oldValue ? amountOfCubes : oldValue);
+            }
+            sumOfAllPowerlevels += minimumCubesRequired.values().stream().reduce(Math::multiplyExact).orElseThrow();
+        }
+        System.out.println("The sum of all power levels is " + sumOfAllPowerlevels);
+        return sumOfAllPowerlevels;
+    }
+
+    public int getDayNumber() {
+        return 2;
+    }
+}
---
+++ src/test/java/nl/th8/adventofcode2022/DayTwoTest.java
@@ -0,0 +1,43 @@
+package nl.th8.adventofcode2022;
+
+import nl.th8.adventofcode2022.utils.PuzzleInputParser;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DayTwoTest {
+
+    @Mock
+    private PuzzleInputParser puzzleInputParser;
+
+    @InjectMocks
+    private DayTwo dayTwo;
+
+    @Test
+    void solvePartOne() {
+        when(puzzleInputParser.getInputAsStringList()).thenReturn(Arrays.asList("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"));
+        assertEquals(8, dayTwo.solvePartOne());
+    }
+
+    @Test
+    void solvePart2() {
+        when(puzzleInputParser.getInputAsStringList()).thenReturn(Arrays.asList("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"));
+        assertEquals(2286, dayTwo.solvePartTwo());
+    }
+}
